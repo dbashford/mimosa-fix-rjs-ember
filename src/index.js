d3ac38579dbd36516ba0067dea5389a9d871fe25
david.bashford <dbashford@bericotechnologies.com>
2014-07-24 10:06:57 -0400

    adding other versions of ember

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ var path = require( "path" );
 var _tweakEmber = function( mimosaConfig, options, next ) {
   if (options.files && options.files.length) {
     options.files.forEach( function( file ) {
-      if (path.basename(file.inputFileName) === "ember.js") {
+      if (["ember.js", "ember.min.js", "ember.prod.js"].indexOf(path.basename(file.inputFileName)) !== -1 ) {
         file.inputFileText =
           file.inputFileText.replace( /define\("ember",/, "define(\"ember-int\"," )
-            .replace(/requireModule\("ember"\)/, "requireModule(\"ember-int\")");        
+            .replace(/requireModule\("ember"\)/, "requireModule(\"ember-int\")");
       }
     });
   }
